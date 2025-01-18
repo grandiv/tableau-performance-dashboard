8c505134ebd0df9b9d7cba02fe2eaab92f26877d
alexchtst-01 <acinatra@gmail.com>
2025-01-18 09:43:52 +0700

    feat: cut off the unescesary DropdownMenuGroup in nav user

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -81,28 +81,13 @@ export function NavUser({
               </div>
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <Sparkles />
-                Upgrade to Pro
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
+            {/* <DropdownMenuGroup>
               <DropdownMenuItem>
                 <BadgeCheck />
                 Account
               </DropdownMenuItem>
-              <DropdownMenuItem>
-                <CreditCard />
-                Billing
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Bell />
-                Notifications
-              </DropdownMenuItem>
             </DropdownMenuGroup>
-            <DropdownMenuSeparator />
+            <DropdownMenuSeparator /> */}
             <DropdownMenuItem
               onClick={() => signOut({ callbackUrl: "/login" })}>
               <LogOut />
