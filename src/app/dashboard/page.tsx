2383e6aa4b0f3d01ca19b6b8bf58d20ae3129c57
Grandiv <grandivfarand@gmail.com>
2025-01-20 17:00:22 +0700

    sidebar functionalities

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -14,13 +14,16 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
-import TableauEmbed from "@/components/tableau";
+// import TableauEmbed from "@/components/tableau";
 import TableauPublicEmbed from "@/components/TableauPublicEmbed";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { useState } from "react";
+import { data } from "@/components/app-sidebar";
 
 export default function Page() {
-  const vizUrl = process.env.NEXT_PUBLIC_VIZ_URL as string;
-
+  const [selectedVizUrl, setSelectedVizUrl] = useState<string | null>(
+    data.navMain[0].vizUrl ?? null
+  );
   const { status } = useAuth();
 
   if (status === "loading") {
@@ -29,7 +32,7 @@ export default function Page() {
 
   return (
     <SidebarProvider>
-      <AppSidebar />
+      <AppSidebar onSelect={(vizUrl) => setSelectedVizUrl(vizUrl)} />
       <SidebarInset>
         <header className="flex justify-between pr-5 h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
@@ -51,7 +54,7 @@ export default function Page() {
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           <div className="auto-rows-min gap-4 md:grid-cols-3">
-            <TableauPublicEmbed vizUrl={vizUrl} />
+            {selectedVizUrl && <TableauPublicEmbed vizUrl={selectedVizUrl} />}
 
             {/* <TableauEmbed
               tableauUrl={
