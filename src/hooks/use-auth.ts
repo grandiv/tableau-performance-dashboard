73aee010229783e31d9da191ebfcf96303fb4e2e
melvinwaluyo <melvinwaluyo@mail.ugm.ac.id>
2025-01-20 13:12:30 +0700

    fix: logo visibility

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -15,10 +15,7 @@ export function useAuth() {
     if (!session) {
       router.replace("/login");
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 49a8a8b1062bf70743738d8f60c0fce50b7ebca6
+    
     if (session) {
       router.push("/dashboard");
     }
